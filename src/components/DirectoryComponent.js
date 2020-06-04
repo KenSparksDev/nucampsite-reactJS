b6288214862f6213d6178c596dc7f5738d816989
Ken Sparks <kensparks.dev@gmail.com>
2020-06-03 21:39:31 -0400

    Presentational and Container Components

---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
+import CampsiteInfo from './CampsiteInfoComponent';
+class Directory extends Component {
+	renderComments(comments) {}
+	render() {
+		const directory = this.props.campsites.map((campsite) => {
+			return (
+				<div key={campsite.id} className="col-md-5 m-1">
+					<Card onClick={() => this.props.onClick(campsite.id)}>
+						<CardImg width="100%" src={campsite.image} alt={campsite.name} />
+						<CardImgOverlay >
+							<CardTitle className="card-img-overlay">{campsite.name}</CardTitle>
+						</CardImgOverlay>
+					</Card>
+				</div>
+			);
+		});
+
+		return (
+			<div className="container">
+				<div className="row">{directory}</div>
+				<CampsiteInfo campsite={this.props.selectedCampsite} />
+			</div>
+		);
+	}
+}
+
+export default Directory;
